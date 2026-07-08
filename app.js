cfc3f6bb0e9704cf7a6b1cacc1adaf5316cda431
betel-espoir10 <12345678+betel-espoir10@users.noreply.github.com>
2026-07-08 23:21:50 +0200

    Ajout flah.ejs.css-dans les pages via partials

--- app.js
+++ app.js
@@ -124,10 +124,12 @@ app.use((req, res, next) => {
 // FLASH MESSAGES
 app.use(flash());
 app.use((req, res, next) => {
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
-  res.locals.user = req.session.user || null;
-  next();
+   res.locals.success = req.flash("success");
+   res.locals.error = req.flash("error");
+   res.locals.info=req.flash("info");
+   res.locals.warning=req.flash("warning");
+   res.locals.user = req.session.user || null;
+   next();
 });
 
 // ROUTES
